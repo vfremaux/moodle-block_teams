c9497353d42278529034d72d8cd7521dd8459eee
valery <valery.fremaux@gmail.com>
2016-10-23 00:49:46 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
+ * Version details.
  *
  * @package    block_teams
  * @category   blocks
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2015062400;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2012120300;        // Requires this Moodle version.
+$plugin->requires  = 2014050800;        // Requires this Moodle version.
 $plugin->component = 'block_teams'; // Full name of the plugin (used for diagnostics).
 $plugin->release = '2.7.0 (Build 2015011108)';
 $plugin->maturity = MATURITY_STABLE;
