38330bfbf61c0a82cedf5fa56cb89a63feaaea0d
valery <valery.fremaux@gmail.com>
2018-10-08 16:49:40 +0200

    Automated sync from SVN

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -16,6 +16,9 @@
 
 namespace block_teams\privacy;
 
+use \core_privacy\local\request\writer;
+use \core_privacy\local\metadata\collection;
+
 defined('MOODLE_INTERNAL') || die();
 
 class provider implements \core_privacy\local\metadata\provider {
@@ -46,6 +49,155 @@ class provider implements \core_privacy\local\metadata\provider {
         $collection->add_database_table('block_teams_requests', $fields, 'privacy:metadata:teams_requests');
 
         return $collection;
+    }
+
+    /**
+     * Get the list of contexts that contain user information for the specified user.
+     *
+     * @param int $userid the userid.
+     * @return contextlist the list of contexts containing user info for the user.
+     */
+    public static function get_contexts_for_userid(int $userid) : contextlist {
+
+        // Fetch all teams courses i am leader in.
+        $sql = "
+            SELECT
+                c.id
+            FROM
+                {context} c,
+                {block_teams} t
+            WHERE
+                c.contextlevel = :contextlevel AND
+                c.instanceid = t.courseid AND
+                t.leaderid = :userid
+        ";
+
+        $params = [
+            'contextlevel'  => CONTEXT_COURSE,
+            'userid'        => $userid,
+        ];
+        $contextlist = new contextlist();
+        $contextlist->add_from_sql($sql, $params);
+
+        // Fetch all teams courses i am invited in.
+        $sql = "
+            SELECT
+                c.id
+            FROM
+                {context} c,
+                {block_teams_invites} i
+            WHERE
+                c.contextlevel = :contextlevel AND
+                c.instanceid = i.courseid AND
+                i.userid = :userid
+        ";
+
+        $params = [
+            'contextlevel'  => CONTEXT_COURSE,
+            'userid'        => $userid,
+        ];
+        $contextlist = new contextlist();
+        $contextlist->add_from_sql($sql, $params);
+
+        // Fetch all teams courses i invite other in.
+        $sql = "
+            SELECT
+                c.id
+            FROM
+                {context} c,
+                {block_teams_invites} i
+            WHERE
+                c.contextlevel = :contextlevel AND
+                c.instanceid = i.courseid AND
+                i.fromuserid = :userid
+        ";
+
+        $params = [
+            'contextlevel'  => CONTEXT_COURSE,
+            'userid'        => $userid,
+        ];
+        $contextlist = new contextlist();
+        $contextlist->add_from_sql($sql, $params);
+
+        // Fetch all teams courses i hve requests for membership.
+        $sql = "
+            SELECT
+                c.id
+            FROM
+                {context} c,
+                {block_teams_requests} r
+            WHERE
+                c.contextlevel = :contextlevel AND
+                c.instanceid = r.courseid AND
+                r.userid = :userid
+        ";
+
+        $params = [
+            'contextlevel'  => CONTEXT_COURSE,
+            'userid'        => $userid,
+        ];
+        $contextlist = new contextlist();
+        $contextlist->add_from_sql($sql, $params);
+
+        return $contextlist;
+    }
+
+    /**
+     * Export all user data for the specified user, in the specified contexts, using the supplied exporter instance.
+     *
+     * @param   approved_contextlist    $contextlist    The approved contexts to export information for.
+     */
+    public static function export_user_data(approved_contextlist $contextlist) {
+
+        $user = $contextlist->get_user();
+
+        foreach ($contextlist->get_contexts() as $ctx) {
+        }
+    }
+
+    public static function delete_data_for_all_users_in_context(deletion_criteria $criteria) {
+        global $DB;
+
+        $context = $criteria->get_context();
+        if (empty($context)) {
+            return;
+        }
+
+        $DB->delete_records('block_teams', ['courseid' => $context->instanceid]);
+        $DB->delete_records('block_teams_invites', ['courseid' => $context->instanceid]);
+        $DB->delete_records('block_teams_requests', ['courseid' => $context->instanceid]);
+    }
+
+    public static function delete_data_for_user(approved_contextlist $contextlist) {
+        global $DB;
+
+        if (empty($contextlist->count())) {
+            return;
+        }
+        $userid = $contextlist->get_user()->id;
+        foreach ($contextlist->get_contexts() as $ctx) {
+
+            // Get all teams the user is leader in.
+            $leadedgroups = $DB->get_records('block_teams', ['courseid' => $ctx->instanceid, 'leaderid' => $userid]);
+            if ($leadedgroups) {
+                $select = " leaderid != :leaderid AND groupid = :groupid ";
+                foreach ($leadedgroups as $team) {
+                    $params = ['leaderid' => $team->leaderid, 'groupid' => $team->groupid];
+                    $nextmemberarr = $DB->get_records_select('groups_members', $select, $params, 'id', '*', 0, 1);
+                    if ($nextmemberarr) {
+                        $nextmember = array_shift($nextmemberarr);
+                        $DB->set_field('block_teams', 'leaderid', $nextmember->id, ['id' => $team->id]);
+                    } else {
+                        // If no more members. than close the team.
+                    }
+                }
+            }
+
+            // Get all group memnbers to pass the leadership to the first in order.
+            $DB->delete_records('block_teams', ['courseid' => $ctx->instanceid, 'leaderid' => $userid]);
+
+            $DB->delete_records('block_teams_invites', ['courseid' => $ctx->instanceid, 'leaderid' => $userid]);
+        }
     }
 
 }
